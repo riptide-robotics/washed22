2a2d2f62edb0e3ab20853bec8611841863222ccd
alokthakrar <alokthakrar1@gmail.com>
2023-01-28 20:07:31 -0800

    new commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PidTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PidTesting.java
@@ -23,133 +23,74 @@ public class PidTesting extends LinearOpMode {
         clawPos.reset();
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("leftFront");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("leftRear");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("rightFront");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("rightRear");
+       /*DcMotor motorFrontLeft = hardwareMap.dcMotor.get("leftFront");
+       DcMotor motorBackLeft = hardwareMap.dcMotor.get("leftRear");
+       DcMotor motorFrontRight = hardwareMap.dcMotor.get("rightFront");
+       DcMotor motorBackRight = hardwareMap.dcMotor.get("rightRear");*/
         DcMotor slides = hardwareMap.dcMotor.get("slides");
-        Servo servo0 = hardwareMap.servo.get("servo0");
-        Servo servo1 = hardwareMap.servo.get("servo1");
-        slides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+       /*Servo servo0 = hardwareMap.servo.get("servo0");
+       Servo servo1 = hardwareMap.servo.get("servo1");
+       slides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);/*/
         double offset = 0;
         boolean bool = true;
 
         // Reverse thv e right side motors
         // Reverse left motors if you are using NeveRests
-        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        //motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        //motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map
-        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        //BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        //BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         // Technically this is the default, however specifying it is clearer
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        //parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
         // Without this, data retrieving from the IMU throws an exception
-        imu.initialize(parameters);
+        //imu.initialize(parameters);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        while (opModeIsActive()) {
-            switch (state){
-                case OPEN:
-                    if(gamepad2.b){
-                        servo0.setPosition(0);
-                        servo1.setPosition(1);
-                        state = lifestate.CLOSED;
-                    }
-                    break;
-                case CLOSED:
-                    if(gamepad2.a){
-                        servo0.setPosition(0.2);
-                        servo1.setPosition(0.8);
-                        state = lifestate.OPEN;
-                    }
-                    break;
-            }
-            double y = 0.55 * -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = 0.55 * gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = 0.55 * gamepad1.right_stick_x;
-            if(gamepad1.left_bumper)
+
+
+        int kp = 1;
+        int ki = 0;
+        int kd = 0;
+
+        int reference = 500;
+        ElapsedTime timer = new ElapsedTime();
+        int integeralSum = 0;
+        int lasterror = 0;
+
+        if(gamepad2.x)
+        {
+            while(slides.getCurrentPosition() <= 500)
             {
-                y = 1.77 * y;
-                x = 1.77 * x;
-                rx = 1.77 * rx;
+                int encoderPos = slides.getCurrentPosition();
+                int error = reference - encoderPos;
+                double derivative = (error - lasterror) / timer.seconds();
+                integeralSum += (error * timer.seconds());
+                double out = (kp * error) + (ki * integeralSum) + (kd * derivative);
+                slides.setPower(out);
+                lasterror = error;
+                timer.reset();
             }
-            if(gamepad1.right_bumper)
-            {
-                y = 0.66 * y;
-                x = 0.66 * x;
-                rx = 0.66 * rx;
-            }
-            int kp = 1;
-            int ki = 0;
-            int kd = 0;
-
-            int reference = 500;
-            ElapsedTime timer = new ElapsedTime();
-            int integeralSum = 0;
-            int lasterror = 0;
-
-            if(gamepad2.x)
-            {
-                while(slides.getCurrentPosition() <= 500)
-                {
-                    int encoderPos = slides.getCurrentPosition();
-                    int error = reference - encoderPos;
-                    double derivative = (error - lasterror) / timer.seconds();
-                    integeralSum += (error * timer.seconds());
-                    double out = (kp * error) + (ki * integeralSum) + (kd * derivative);
-                    slides.setPower(out);
-                    lasterror = error;
-                    timer.reset();
-                }
-                slides.setPower(0.95);
-            }
-            else if(gamepad2.y)
-            {
-                slides.setPower(-0.96);
-            }
-            else
-            {
-                slides.setPower(0);
-            }
-
-
-
-            // Read inverse IMU heading, as the IMU heading is CW positive
-            double botHeading = -imu.getAngularOrientation().firstAngle;
-            if(gamepad1.a)
-            {
-                offset = -imu.getAngularOrientation().firstAngle;
-            }
-            if(gamepad1.b)
-            {
-                offset = 0;
-            }
-            double rotX = x * Math.cos(botHeading - offset) - y * Math.sin(botHeading - offset);
-            double rotY = x * Math.sin(botHeading - offset) + y * Math.cos(botHeading - offset);
-
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
-
-            motorFrontLeft.setPower(frontLeftPower);
-            motorBackLeft.setPower(backLeftPower);
-            motorFrontRight.setPower(frontRightPower);
-            motorBackRight.setPower(backRightPower);
-            telemetry.addData("y speed:", y);
-            telemetry.addData("x speed:", x);
-            telemetry.addData("left", gamepad1.left_bumper);
-            telemetry.addData("right1", gamepad1.right_bumper);
-            telemetry.update();
+            slides.setPower(0.95);
         }
+
+        else
+        {
+            slides.setPower(0);
+        }
+        //sorry my bad2
+
+
+        // Read inverse IMU heading, as the IMU heading is CW positive
+
+        telemetry.addData("left", gamepad1.left_bumper);
+        telemetry.addData("right1", gamepad1.right_bumper);
+        telemetry.update();
     }
 }
+
+
